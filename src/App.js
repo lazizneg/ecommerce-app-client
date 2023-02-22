92e8fdcbc2ad09dcc8c5f4a0abfc4d750cfc6cd1
Lazizjon <ln41128n@pace.edu>
2023-02-22 10:18:32 -0500

    working on navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { useEffect } from "react";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Home from "./scenes/home/Home";
-import ItemDetails from "./scenes/itemDetails/ItemDetails";
+import itemDetails from "./scenes/itemDetails/itemDetails";
 import Checkout from "./scenes/checkout/Checkout";
 import Confirmation from "./scenes/checkout/Confirmation";
+import Navbar from "./scenes/global/Navbar";
 
 const ScrollToTop = () => {
     const { pathname } = useLocation();
@@ -19,10 +20,11 @@ function App() {
     return (
         <div className="app">
             <BrowserRouter>
+                <Navbar />
                 <ScrollToTop />
                 <Routes>
                     <Route path="/" element={<Home />} />
-                    <Route path="item/:itemId" element={<ItemDetails />} />
+                    <Route path="item/:itemId" element={<itemDetails />} />
                     <Route path="checkout" element={<Checkout />} />
                     <Route path="checkout/success" element={<Confirmation />} />
                 </Routes>
